ec57ca30f9ab8ebcfe1d21a103a192804aa5840e
Andrew Westgerdes <ajestgerdes@yahoo.com>
2019-04-08 22:20:17 -0400

    fixed issue in signup page

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 
 export class Signup extends Component {
+
     render() {
         return (
             <div class="container">
