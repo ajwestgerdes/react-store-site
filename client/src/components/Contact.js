3aabc57a18d2ef5d55ee5d7d522dc9b368eb06ac
Andrew Westgerdes <ajwestgerdes@gmail.com>
2019-06-28 18:37:10 -0400

    updated style stuff

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -11,8 +11,8 @@ export class Contact extends Component {
                     <div className="text-center">9:30AM - 6:00PM</div>
                     <div className="row">
                         <div className="col-lg-3 ml-auto text-center">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3064.498959103403!2d-84.23470688430639!3d39.81822939974368!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88408035fab96965%3A0xd3072afc05e4c271!2s5500+N+Main+St%2C+Dayton%2C+OH+45415!5e0!3m2!1sen!2sus!4v1561688840373!5m2!1sen!2sus"></iframe>
-
+                            <iframe className="gmap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3064.498959103403!2d-84.23470688430639!3d39.81822939974368!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88408035fab96965%3A0xd3072afc05e4c271!2s5500+N+Main+St%2C+Dayton%2C+OH+45415!5e0!3m2!1sen!2sus!4v1561688840373!5m2!1sen!2sus"></iframe>
+                            <a href="https://www.google.com/maps?ll=39.818225,-84.232518&z=15&t=m&hl=en&gl=US&mapclient=embed&q=5500+N+Main+St+Dayton,+OH+45415">5500 North Main Street, Dayton OH 45415</a>
                         </div>
                         <div className="col-lg-3 ml-auto text-center">
                             <div>937-275-4911</div>
