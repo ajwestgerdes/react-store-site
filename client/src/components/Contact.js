12fcd271adf192bde7de7af09715c8f22ba8c803
Andrew Westgerdes <ajwestgerdes@gmail.com>
2019-06-27 21:07:36 -0400

    map fully functional, needs style

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
-import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps'
+import { GoogleMap, withScriptjs, withGoogleMap, Marker } from 'react-google-maps'
 
-function GMap() {
+const WrappedMap = withScriptjs(withGoogleMap((props) =>
     <GoogleMap
-        defaultZoom={10}
-        defaultCenter={{ lat: 45, lng: -75 }}
-    />
-}
-
-const WrappedMap = withScriptjs(withGoogleMap(GMap));
+        defaultZoom={17}
+        defaultCenter={{ lat: 39.818270, lng: -84.232538 }}
+    >
+        {props.isMarkerShown && <Marker position={{ lat: 39.818270, lng: -84.232538 }} />}
+    </GoogleMap>
+))
 
 export class Contact extends Component {
     render() {
@@ -20,16 +20,25 @@ export class Contact extends Component {
                     <div className="text-center">Monday - Saturday</div>
                     <div className="text-center">9:30AM - 6:00PM</div>
                     <div className="row">
-                        <div className="col-lg-4 ml-auto text-center">
+                        <div className="col-lg-3 ml-auto text-center">
+                            <WrappedMap isMarkerShown
+                                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyD5H7UUEe1cnSKK6lukPa9Fsr95Lq0oRW8"
+                                loadingElement={<div style={{ height: `50%` }} />}
+                                containerElement={<div style={{ height: `300px` }} />}
+                                mapElement={<div style={{ height: `100%` }} />} />
+                        </div>
+                        <div className="col-lg-3 ml-auto text-center">
                             <div>937-275-4911</div>
                         </div>
-                        <div className="col-lg-4 mr-auto text-center">
+                        <div className="col-lg-3 mr-auto text-center">
                             <div className="d-block">shoes@gmail.com</div>
                         </div>
-                        <WrappedMap googleMapURL={"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"} />
+                        <div style={{ width: "100vw", height: "100v" }}>
+
+                        </div>
                     </div>
                 </section>
-            </div>
+            </div >
         )
     }
 }
